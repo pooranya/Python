91e82184fe5716a6de7b7c21d8e443c9a486383a
pooranya <52210652+pooranya@users.noreply.github.com>
2020-02-09 14:21:34 +0530

    Update infy question swetha.py

--- infy question swetha.py
+++ infy question swetha.py
@@ -1,4 +1,4 @@
-n="Sweth129@001a@@"
+n="Sweth1229@0100@a@"
 l1=[]
 l2=[]
 l3=[]
@@ -22,10 +22,45 @@ for i in range(d):
     else:
         o.append(l4[i])
 s=""
-if c%2==0:
+if len(e)==len(o) and c%2==0:
     for i in range(0,len(e)):
         s=s+str(e[i])+str(o[i])
-else:
+    print(s)
+elif len(e)==len(o) and c%2!=0: 
     for i in range(0,len(o)):
         s=s+str(o[i])+str(e[i])
 print(s)
+z=""
+if len(e)>len(o) and c%2==0:
+    for i in range(0,len(o)):
+        z=z+str(e[i])+str(o[i])
+        e1=e[len(o):]
+        z1=list(z)
+        for i in range(len(e1)):
+            z1.append(str(e1[i]))
+    print("".join(z1))
+elif len(e)>len(o) and c%2!=0: 
+    for i in range(0,len(o)):
+        z=z+str(o[i])+str(e[i])
+        e1=e[len(o):]
+        z1=list(z)
+        for i in range(len(e1)):
+            z1.append(str(e1[i]))
+    print("".join(z1))
+if len(o)>len(e) and c%2==0:
+    for i in range(0,len(e)):
+        z=z+str(e[i])+str(o[i])
+        o1=o[len(e):]
+        z1=list(z)
+        for i in range(len(o1)):
+            z1.append(str(o1[i]))
+    print("".join(z1))
+elif len(o)>len(e) and c%2!=0: 
+    for i in range(0,len(e)):
+        z=z+str(o[i])+str(e[i])
+        o1=o[len(e):]
+        z1=list(z)
+        for i in range(len(o1)):
+            z1.append(str(o1[i]))
+    print("".join(z1))
+    
