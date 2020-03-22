be2fbedb7175c5e4de98e51aa4e5296f3d4a39d0
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-22 09:40:51 +0530

    Create Division 11.py

---
+++ Division 11.py
@@ -0,0 +1,34 @@
+#ans
+a,b=list(map(int,input().split()))
+c=int(a)//int(b)
+d=int(a)%int(b)
+print(a,"/",b,"=",c)
+print(a,"%",b,"=",d)
+
+
+
+
+#que
+Write a program to divide two numbers, print the quotient and remainder – without the division operation ?
+
+Division is one of the basic arithmetic operations; each part of a division equation has a name. The three main names are the dividend, the divisor, and the quotient. a. Dividend - The dividend is the number you are dividing up b. Divisor - The divisor is the number you are dividing by c. Quotient - The quotient is the answer d. Remainder – The left over after the division, the group that is incomplete Dividend ÷ Divisor = Quotient + Remainder
+
+Input Format
+
+Input will be an Integer
+
+Constraints
+
+1= n1 = 2,147,483,647
+
+Output Format
+
+%d / %d = %d %d % %d = %d
+
+Sample Input 0
+
+12 5
+Sample Output 0
+
+12 / 5 = 2
+12 % 5 = 2
