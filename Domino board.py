7b64450a30e84bf1aff56067c0a32e756c37b8e0
pooranya <52210652+pooranya@users.noreply.github.com>
2020-03-22 10:14:13 +0530

    Create Domino board.py

---
+++ Domino board.py
@@ -0,0 +1,29 @@
+#ans
+a,b=list(map(int,input().split()))
+c=(a*b)//2
+print(c)
+
+
+
+
+#que
+You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions: Each domino completely covers two squares. No two dominoes overlap. Each domino lies entirely inside the board. It is allowed to touch the edges of the board. Find the maximum number of dominoes, which can be placed under these restrictions.
+
+Input Format
+
+Input contains 2 integer
+
+Constraints
+
+1<=m<=1000,1<=n<=1000
+
+Output Format
+
+Print the maximal number of dominoes
+
+Sample Input 0
+
+2 4
+Sample Output 0
+
+4
